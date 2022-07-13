f11eac769d2f5f9b5e74615db898321f55c3d721
qingsong <qingsong@qq.com>
2022-07-13 14:59:03 +0800

    Log4j 与 slf4j 集成。

--- src/main/java/org/example/slf4j/Slf4jLog4j2App.java
+++ src/main/java/org/example/slf4j/Slf4jLog4j2App.java
@@ -7,6 +7,11 @@ import org.slf4j.LoggerFactory;
 /**
  * 现在使用的是slf4j的接口，但是是通过桥接器最终通过log4j进行实际输出。门面提供了统一接口，屏蔽了底层实现。也是面向对象的体现。
  *
+ *     <dependency>
+ *       <groupId>log4j</groupId>
+ *       <artifactId>log4j</artifactId>
+ *       <version>1.2.17</version>
+ *     </dependency>
  * <!-- slf4j 兼容 log4j 1.x 桥接-->
  *     <dependency>
  *       <groupId>org.slf4j</groupId>
